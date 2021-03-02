7436ec644c00c743a8ccde3d2ecba63bf2b1cfa7
Michel Megens <michel@michelmegens.net>
2021-03-02 16:49:35 +0100

    tests: test parsing of different telegrams
    Unit test the parsing of:
    
    	- DSMR 2
    	- DSMR 4.2
    	- ESMR 5
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- SensateIoT.SmartEnergy.Dsmr.Parser.Tests/TelegramParser/ParserTests.cs
+++ SensateIoT.SmartEnergy.Dsmr.Parser.Tests/TelegramParser/ParserTests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.IO;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using SensateIoT.SmartEnergy.Dsmr.Parser.Common.Models;
 
 namespace SensateIoT.SmartEnergy.Dsmr.Parser.Tests.TelegramParser
 {
@@ -10,26 +13,47 @@ namespace SensateIoT.SmartEnergy.Dsmr.Parser.Tests.TelegramParser
         [TestMethod]
         public void CanParseV2()
         {
+	        var input = getTelegram("2");
+			var parser = new Common.Services.Parser();
+			var result = parser.Parse(input).GetAwaiter().GetResult();
+
+			Assert.AreEqual(ObisVersion.V20, result.MessageVersion);
+			Assert.AreEqual(0.98M, result.InstantaneousPowerUsage);
+			Assert.AreEqual(185M, result.EnergyConsumptionTariff1);
+			Assert.AreEqual(13M, result.EnergyProductionTariff1);
         }
 
         [TestMethod]
         public void CanParseV4()
         {
-        }
+	        var input = getTelegram("4");
+	        var parser = new Common.Services.Parser();
+	        var result = parser.Parse(input).GetAwaiter().GetResult();
+
+	        Assert.AreEqual(ObisVersion.V42, result.MessageVersion);
+	        Assert.AreEqual(0.494M, result.InstantaneousPowerUsage);
+	        Assert.AreEqual(2074.842M, result.EnergyConsumptionTariff1);
+	        Assert.AreEqual(10.981M, result.EnergyProductionTariff1);
+		}
 
         [TestMethod]
         public void CanParseV5()
         {
-	        var current = Environment.CurrentDirectory;
-	        var v5File = $"{current}{Path.DirectorySeparatorChar}Resources{Path.DirectorySeparatorChar}v5.txt";
-	        var input = File.ReadAllText(v5File);
-		        
+	        var input = getTelegram("5");
 			var parser = new Common.Services.Parser();
 			var result = parser.Parse(input).GetAwaiter().GetResult();
 
 			Assert.AreEqual(2745.056M, result.GasConsumption);
 			Assert.AreEqual(0.306M, result.InstantaneousPowerProduction);
 			Assert.AreEqual(2055.686M, result.EnergyConsumptionTariff2);
+			Assert.AreEqual(ObisVersion.V50, result.MessageVersion);
+        }
+
+        private static string getTelegram(string version)
+        {
+	        var current = Environment.CurrentDirectory;
+	        var v5File = $"{current}{Path.DirectorySeparatorChar}Resources{Path.DirectorySeparatorChar}v{version}.txt";
+	        return File.ReadAllText(v5File);
         }
     }
 }
