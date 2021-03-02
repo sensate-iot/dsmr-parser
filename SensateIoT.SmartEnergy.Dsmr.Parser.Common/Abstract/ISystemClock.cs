6f2aedfc56c7a269e33edb542be1cb6b6cf41297
Michel Megens <michel@michelmegens.net>
2021-03-02 21:42:38 +0100

    common: add system clock interface
    Add a system clock interface to improve the testability of the code.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

---
+++ SensateIoT.SmartEnergy.Dsmr.Parser.Common/Abstract/ISystemClock.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SensateIoT.SmartEnergy.Dsmr.Parser.Common.Abstract
+{
+	public interface ISystemClock
+	{
+		DateTime GetNowUtc();
+	}
+}
---
+++ SensateIoT.SmartEnergy.Dsmr.Parser.Common/Services/SystemClock.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SensateIoT.SmartEnergy.Dsmr.Parser.Common.Abstract;
+
+namespace SensateIoT.SmartEnergy.Dsmr.Parser.Common.Services
+{
+	public class SystemClock : ISystemClock
+	{
+		public DateTime GetNowUtc()
+		{
+			return DateTime.UtcNow;
+		}
+	}
+}
